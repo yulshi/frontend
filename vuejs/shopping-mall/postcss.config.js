ddd3fa6583deef98bfb8c57b17c0e8337d4ca31e
Yulong Shi <yu.long.shi@oracle.com>
2020-07-17 20:39:48 +0800

    add postcss-px-to-viewport

---
+++ vuejs/shopping-mall/postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    'postcss-px-to-viewport': {
+      viewportWidth: 375,
+      viewportHeight: 667,
+      unitPrecision: 5,
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: ['ignore', 'tabbar', 'tabbar-item'],
+      minPixelValue: 1,
+      mediaQuery: false
+    }
+  }
+}
